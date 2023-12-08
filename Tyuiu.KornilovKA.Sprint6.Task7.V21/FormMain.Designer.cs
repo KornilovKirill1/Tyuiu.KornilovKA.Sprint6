b29e05ee9520c11bc3c01b5b55fad19de30fb8db
kilianAid <kilian_bs@mail.ru>
2023-12-08 07:45:21 +0500

    Task7

---
+++ Tyuiu.KornilovKA.Sprint6.Task7.V21/FormMain.Designer.cs
@@ -0,0 +1,323 @@
+﻿
+namespace Tyuiu.KornilovKA.Sprint6.Task7.V21
+{
+    partial class FormMain
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(FormMain));
+            this.panelButtons_KKA = new System.Windows.Forms.Panel();
+            this.panelTask_KKA = new System.Windows.Forms.Panel();
+            this.groupBoxTask_KKA = new System.Windows.Forms.GroupBox();
+            this.textBoxTask_KKA = new System.Windows.Forms.TextBox();
+            this.panelInputMatrix_KKA = new System.Windows.Forms.Panel();
+            this.groupBoxInputMatrix_KKA = new System.Windows.Forms.GroupBox();
+            this.dataGridViewInput_KKA = new System.Windows.Forms.DataGridView();
+            this.splitterBetweenInputAndOutput_KKA = new System.Windows.Forms.Splitter();
+            this.panelOutputMatrix_KKA = new System.Windows.Forms.Panel();
+            this.groupBoxOutputMatrix_KKA = new System.Windows.Forms.GroupBox();
+            this.dataGridViewOutput_KKA = new System.Windows.Forms.DataGridView();
+            this.toolTipHelpButtons_KKA = new System.Windows.Forms.ToolTip(this.components);
+            this.openFileDialogTask_KKA = new System.Windows.Forms.OpenFileDialog();
+            this.saveFileDialogMatrix_KKA = new System.Windows.Forms.SaveFileDialog();
+            this.buttonSave_KKA = new System.Windows.Forms.Button();
+            this.buttonInfo_KKA = new System.Windows.Forms.Button();
+            this.buttonDone_KKA = new System.Windows.Forms.Button();
+            this.buttonOpenFile_KKA = new System.Windows.Forms.Button();
+            this.panelButtons_KKA.SuspendLayout();
+            this.panelTask_KKA.SuspendLayout();
+            this.groupBoxTask_KKA.SuspendLayout();
+            this.panelInputMatrix_KKA.SuspendLayout();
+            this.groupBoxInputMatrix_KKA.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridViewInput_KKA)).BeginInit();
+            this.panelOutputMatrix_KKA.SuspendLayout();
+            this.groupBoxOutputMatrix_KKA.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridViewOutput_KKA)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // panelButtons_KKA
+            // 
+            this.panelButtons_KKA.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.panelButtons_KKA.Controls.Add(this.buttonSave_KKA);
+            this.panelButtons_KKA.Controls.Add(this.buttonInfo_KKA);
+            this.panelButtons_KKA.Controls.Add(this.buttonDone_KKA);
+            this.panelButtons_KKA.Controls.Add(this.buttonOpenFile_KKA);
+            this.panelButtons_KKA.Dock = System.Windows.Forms.DockStyle.Top;
+            this.panelButtons_KKA.Location = new System.Drawing.Point(0, 0);
+            this.panelButtons_KKA.Name = "panelButtons_KKA";
+            this.panelButtons_KKA.Size = new System.Drawing.Size(884, 66);
+            this.panelButtons_KKA.TabIndex = 1;
+            // 
+            // panelTask_KKA
+            // 
+            this.panelTask_KKA.Controls.Add(this.groupBoxTask_KKA);
+            this.panelTask_KKA.Dock = System.Windows.Forms.DockStyle.Top;
+            this.panelTask_KKA.Location = new System.Drawing.Point(0, 66);
+            this.panelTask_KKA.Name = "panelTask_KKA";
+            this.panelTask_KKA.Padding = new System.Windows.Forms.Padding(5);
+            this.panelTask_KKA.Size = new System.Drawing.Size(884, 68);
+            this.panelTask_KKA.TabIndex = 2;
+            // 
+            // groupBoxTask_KKA
+            // 
+            this.groupBoxTask_KKA.Controls.Add(this.textBoxTask_KKA);
+            this.groupBoxTask_KKA.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.groupBoxTask_KKA.Location = new System.Drawing.Point(5, 5);
+            this.groupBoxTask_KKA.Name = "groupBoxTask_KKA";
+            this.groupBoxTask_KKA.Size = new System.Drawing.Size(874, 58);
+            this.groupBoxTask_KKA.TabIndex = 0;
+            this.groupBoxTask_KKA.TabStop = false;
+            this.groupBoxTask_KKA.Text = "Условие:";
+            // 
+            // textBoxTask_KKA
+            // 
+            this.textBoxTask_KKA.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.textBoxTask_KKA.BackColor = System.Drawing.SystemColors.Control;
+            this.textBoxTask_KKA.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.textBoxTask_KKA.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.textBoxTask_KKA.Location = new System.Drawing.Point(7, 20);
+            this.textBoxTask_KKA.Multiline = true;
+            this.textBoxTask_KKA.Name = "textBoxTask_KKA";
+            this.textBoxTask_KKA.ReadOnly = true;
+            this.textBoxTask_KKA.Size = new System.Drawing.Size(861, 32);
+            this.textBoxTask_KKA.TabIndex = 0;
+            this.textBoxTask_KKA.TabStop = false;
+            this.textBoxTask_KKA.Text = resources.GetString("textBoxTask_KKA.Text");
+            // 
+            // panelInputMatrix_KKA
+            // 
+            this.panelInputMatrix_KKA.Controls.Add(this.groupBoxInputMatrix_KKA);
+            this.panelInputMatrix_KKA.Dock = System.Windows.Forms.DockStyle.Left;
+            this.panelInputMatrix_KKA.Location = new System.Drawing.Point(0, 134);
+            this.panelInputMatrix_KKA.Name = "panelInputMatrix_KKA";
+            this.panelInputMatrix_KKA.Padding = new System.Windows.Forms.Padding(2);
+            this.panelInputMatrix_KKA.Size = new System.Drawing.Size(439, 377);
+            this.panelInputMatrix_KKA.TabIndex = 3;
+            // 
+            // groupBoxInputMatrix_KKA
+            // 
+            this.groupBoxInputMatrix_KKA.Controls.Add(this.dataGridViewInput_KKA);
+            this.groupBoxInputMatrix_KKA.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.groupBoxInputMatrix_KKA.Location = new System.Drawing.Point(2, 2);
+            this.groupBoxInputMatrix_KKA.Name = "groupBoxInputMatrix_KKA";
+            this.groupBoxInputMatrix_KKA.Size = new System.Drawing.Size(435, 373);
+            this.groupBoxInputMatrix_KKA.TabIndex = 0;
+            this.groupBoxInputMatrix_KKA.TabStop = false;
+            this.groupBoxInputMatrix_KKA.Text = "Ввод:";
+            // 
+            // dataGridViewInput_KKA
+            // 
+            this.dataGridViewInput_KKA.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridViewInput_KKA.ColumnHeadersVisible = false;
+            this.dataGridViewInput_KKA.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.dataGridViewInput_KKA.Location = new System.Drawing.Point(3, 16);
+            this.dataGridViewInput_KKA.Name = "dataGridViewInput_KKA";
+            this.dataGridViewInput_KKA.ReadOnly = true;
+            this.dataGridViewInput_KKA.RowHeadersVisible = false;
+            this.dataGridViewInput_KKA.Size = new System.Drawing.Size(429, 354);
+            this.dataGridViewInput_KKA.TabIndex = 0;
+            this.dataGridViewInput_KKA.TabStop = false;
+            // 
+            // splitterBetweenInputAndOutput_KKA
+            // 
+            this.splitterBetweenInputAndOutput_KKA.Location = new System.Drawing.Point(439, 134);
+            this.splitterBetweenInputAndOutput_KKA.Name = "splitterBetweenInputAndOutput_KKA";
+            this.splitterBetweenInputAndOutput_KKA.Size = new System.Drawing.Size(3, 377);
+            this.splitterBetweenInputAndOutput_KKA.TabIndex = 4;
+            this.splitterBetweenInputAndOutput_KKA.TabStop = false;
+            // 
+            // panelOutputMatrix_KKA
+            // 
+            this.panelOutputMatrix_KKA.Controls.Add(this.groupBoxOutputMatrix_KKA);
+            this.panelOutputMatrix_KKA.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.panelOutputMatrix_KKA.Location = new System.Drawing.Point(442, 134);
+            this.panelOutputMatrix_KKA.Name = "panelOutputMatrix_KKA";
+            this.panelOutputMatrix_KKA.Padding = new System.Windows.Forms.Padding(2);
+            this.panelOutputMatrix_KKA.Size = new System.Drawing.Size(442, 377);
+            this.panelOutputMatrix_KKA.TabIndex = 5;
+            // 
+            // groupBoxOutputMatrix_KKA
+            // 
+            this.groupBoxOutputMatrix_KKA.Controls.Add(this.dataGridViewOutput_KKA);
+            this.groupBoxOutputMatrix_KKA.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.groupBoxOutputMatrix_KKA.Location = new System.Drawing.Point(2, 2);
+            this.groupBoxOutputMatrix_KKA.Name = "groupBoxOutputMatrix_KKA";
+            this.groupBoxOutputMatrix_KKA.Size = new System.Drawing.Size(438, 373);
+            this.groupBoxOutputMatrix_KKA.TabIndex = 0;
+            this.groupBoxOutputMatrix_KKA.TabStop = false;
+            this.groupBoxOutputMatrix_KKA.Text = "Вывод:";
+            // 
+            // dataGridViewOutput_KKA
+            // 
+            this.dataGridViewOutput_KKA.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridViewOutput_KKA.ColumnHeadersVisible = false;
+            this.dataGridViewOutput_KKA.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.dataGridViewOutput_KKA.Location = new System.Drawing.Point(3, 16);
+            this.dataGridViewOutput_KKA.Name = "dataGridViewOutput_KKA";
+            this.dataGridViewOutput_KKA.ReadOnly = true;
+            this.dataGridViewOutput_KKA.RowHeadersVisible = false;
+            this.dataGridViewOutput_KKA.Size = new System.Drawing.Size(432, 354);
+            this.dataGridViewOutput_KKA.TabIndex = 0;
+            this.dataGridViewOutput_KKA.TabStop = false;
+            // 
+            // toolTipHelpButtons_KKA
+            // 
+            this.toolTipHelpButtons_KKA.IsBalloon = true;
+            this.toolTipHelpButtons_KKA.ToolTipIcon = System.Windows.Forms.ToolTipIcon.Info;
+            this.toolTipHelpButtons_KKA.ToolTipTitle = "Подсказка";
+            // 
+            // openFileDialogTask_KKA
+            // 
+            this.openFileDialogTask_KKA.FileName = "openFileDialog1";
+            // 
+            // buttonSave_KKA
+            // 
+            this.buttonSave_KKA.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.buttonSave_KKA.Enabled = false;
+            this.buttonSave_KKA.FlatAppearance.BorderSize = 2;
+            this.buttonSave_KKA.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.buttonSave_KKA.ForeColor = System.Drawing.Color.Silver;
+            this.buttonSave_KKA.Image = global::Tyuiu.KornilovKA.Sprint6.Task7.V21.Properties.Resources.page_save;
+            this.buttonSave_KKA.Location = new System.Drawing.Point(168, 4);
+            this.buttonSave_KKA.Name = "buttonSave_KKA";
+            this.buttonSave_KKA.Size = new System.Drawing.Size(75, 55);
+            this.buttonSave_KKA.TabIndex = 3;
+            this.buttonSave_KKA.TabStop = false;
+            this.toolTipHelpButtons_KKA.SetToolTip(this.buttonSave_KKA, "Сохранить обработанные данные в файл CSV");
+            this.buttonSave_KKA.UseVisualStyleBackColor = true;
+            this.buttonSave_KKA.Click += new System.EventHandler(this.buttonSave_KKA_Click);
+            this.buttonSave_KKA.MouseEnter += new System.EventHandler(this.buttonSave_KKA_MouseEnter);
+            // 
+            // buttonInfo_KKA
+            // 
+            this.buttonInfo_KKA.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.buttonInfo_KKA.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Center;
+            this.buttonInfo_KKA.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.buttonInfo_KKA.FlatAppearance.BorderSize = 2;
+            this.buttonInfo_KKA.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.buttonInfo_KKA.ForeColor = System.Drawing.Color.Silver;
+            this.buttonInfo_KKA.Image = global::Tyuiu.KornilovKA.Sprint6.Task7.V21.Properties.Resources.help;
+            this.buttonInfo_KKA.Location = new System.Drawing.Point(804, 4);
+            this.buttonInfo_KKA.Name = "buttonInfo_KKA";
+            this.buttonInfo_KKA.Size = new System.Drawing.Size(75, 55);
+            this.buttonInfo_KKA.TabIndex = 2;
+            this.buttonInfo_KKA.TabStop = false;
+            this.toolTipHelpButtons_KKA.SetToolTip(this.buttonInfo_KKA, "Сведения о программе");
+            this.buttonInfo_KKA.UseVisualStyleBackColor = true;
+            this.buttonInfo_KKA.Click += new System.EventHandler(this.buttonInfo_KKA_Click);
+            this.buttonInfo_KKA.MouseEnter += new System.EventHandler(this.buttonInfo_KKA_MouseEnter);
+            // 
+            // buttonDone_KKA
+            // 
+            this.buttonDone_KKA.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Center;
+            this.buttonDone_KKA.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.buttonDone_KKA.Enabled = false;
+            this.buttonDone_KKA.FlatAppearance.BorderSize = 2;
+            this.buttonDone_KKA.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.buttonDone_KKA.ForeColor = System.Drawing.Color.Silver;
+            this.buttonDone_KKA.Image = global::Tyuiu.KornilovKA.Sprint6.Task7.V21.Properties.Resources.page_white_go;
+            this.buttonDone_KKA.Location = new System.Drawing.Point(86, 4);
+            this.buttonDone_KKA.Name = "buttonDone_KKA";
+            this.buttonDone_KKA.Size = new System.Drawing.Size(75, 55);
+            this.buttonDone_KKA.TabIndex = 1;
+            this.buttonDone_KKA.TabStop = false;
+            this.toolTipHelpButtons_KKA.SetToolTip(this.buttonDone_KKA, "Выполнить обработку данных");
+            this.buttonDone_KKA.UseVisualStyleBackColor = true;
+            this.buttonDone_KKA.Click += new System.EventHandler(this.buttonDone_KKA_Click);
+            this.buttonDone_KKA.MouseEnter += new System.EventHandler(this.buttonDone_KKA_MouseEnter);
+            // 
+            // buttonOpenFile_KKA
+            // 
+            this.buttonOpenFile_KKA.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Center;
+            this.buttonOpenFile_KKA.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.buttonOpenFile_KKA.FlatAppearance.BorderSize = 2;
+            this.buttonOpenFile_KKA.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.buttonOpenFile_KKA.ForeColor = System.Drawing.Color.Silver;
+            this.buttonOpenFile_KKA.Image = global::Tyuiu.KornilovKA.Sprint6.Task7.V21.Properties.Resources.folder_page;
+            this.buttonOpenFile_KKA.Location = new System.Drawing.Point(4, 4);
+            this.buttonOpenFile_KKA.Name = "buttonOpenFile_KKA";
+            this.buttonOpenFile_KKA.Size = new System.Drawing.Size(75, 55);
+            this.buttonOpenFile_KKA.TabIndex = 0;
+            this.buttonOpenFile_KKA.TabStop = false;
+            this.toolTipHelpButtons_KKA.SetToolTip(this.buttonOpenFile_KKA, "Открыть файл для обработки данных в формате CSV");
+            this.buttonOpenFile_KKA.UseVisualStyleBackColor = true;
+            this.buttonOpenFile_KKA.Click += new System.EventHandler(this.buttonOpenFile_KKA_Click);
+            this.buttonOpenFile_KKA.MouseEnter += new System.EventHandler(this.buttonOpenFile_KKA_MouseEnter);
+            // 
+            // FormMain
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(884, 511);
+            this.Controls.Add(this.panelOutputMatrix_KKA);
+            this.Controls.Add(this.splitterBetweenInputAndOutput_KKA);
+            this.Controls.Add(this.panelInputMatrix_KKA);
+            this.Controls.Add(this.panelTask_KKA);
+            this.Controls.Add(this.panelButtons_KKA);
+            this.Name = "FormMain";
+            this.Text = "Спринт 6 | Таск 7 | Вариант 21 | Корнилов К.А.";
+            this.WindowState = System.Windows.Forms.FormWindowState.Maximized;
+            this.Load += new System.EventHandler(this.FormMain_Load);
+            this.panelButtons_KKA.ResumeLayout(false);
+            this.panelTask_KKA.ResumeLayout(false);
+            this.groupBoxTask_KKA.ResumeLayout(false);
+            this.groupBoxTask_KKA.PerformLayout();
+            this.panelInputMatrix_KKA.ResumeLayout(false);
+            this.groupBoxInputMatrix_KKA.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridViewInput_KKA)).EndInit();
+            this.panelOutputMatrix_KKA.ResumeLayout(false);
+            this.groupBoxOutputMatrix_KKA.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridViewOutput_KKA)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Panel panelButtons_KKA;
+        private System.Windows.Forms.Button buttonInfo_KKA;
+        private System.Windows.Forms.Button buttonDone_KKA;
+        private System.Windows.Forms.Button buttonOpenFile_KKA;
+        private System.Windows.Forms.Panel panelTask_KKA;
+        private System.Windows.Forms.GroupBox groupBoxTask_KKA;
+        private System.Windows.Forms.TextBox textBoxTask_KKA;
+        private System.Windows.Forms.Panel panelInputMatrix_KKA;
+        private System.Windows.Forms.GroupBox groupBoxInputMatrix_KKA;
+        private System.Windows.Forms.Splitter splitterBetweenInputAndOutput_KKA;
+        private System.Windows.Forms.Panel panelOutputMatrix_KKA;
+        private System.Windows.Forms.GroupBox groupBoxOutputMatrix_KKA;
+        private System.Windows.Forms.DataGridView dataGridViewInput_KKA;
+        private System.Windows.Forms.DataGridView dataGridViewOutput_KKA;
+        private System.Windows.Forms.Button buttonSave_KKA;
+        private System.Windows.Forms.ToolTip toolTipHelpButtons_KKA;
+        private System.Windows.Forms.OpenFileDialog openFileDialogTask_KKA;
+        private System.Windows.Forms.SaveFileDialog saveFileDialogMatrix_KKA;
+    }
+}
+
---
+++ Tyuiu.KornilovKA.Sprint6.Task7.V21/FormMain.cs
@@ -0,0 +1,176 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO;
+
+using Tyuiu.KornilovKA.Sprint6.Task7.V21.Lib;
+
+namespace Tyuiu.KornilovKA.Sprint6.Task7.V21
+{
+    public partial class FormMain : Form
+    {
+        public FormMain()
+        {
+            InitializeComponent();
+            openFileDialogTask_KKA.Filter = "Значения, разделённые запятыми(*.csv)|*csv|Все файлы(*.*)|*.*";
+            saveFileDialogMatrix_KKA.Filter = "Значения, разделённые запятыми(*.csv)|*csv|Все файлы(*.*)|*.*";
+        }
+        static int rows;
+        static int columns;
+        static string openFilePath;
+        DataService ds = new DataService();
+        public static int[,] LoadFromFileData(string filePath)
+        {
+            string fileData = File.ReadAllText(filePath);
+            fileData = fileData.Replace('\n', '\r');
+            string[] lines = fileData.Split(new char[] { '\r' }, StringSplitOptions.RemoveEmptyEntries);
+
+            rows = lines.Length;
+            columns = lines[0].Split(';').Length;
+
+            int[,] arrayValues = new int[rows, columns];
+
+            for (int r = 0; r < rows; r++)
+            {
+                string[] line_r = lines[r].Split(';');
+                for (int c = 0; c < columns; c++)
+                {
+                    arrayValues[r, c] = Convert.ToInt32(line_r[c]);
+                }
+            }
+            return arrayValues;
+        }
+        private void buttonOpenFile_KKA_Click(object sender, EventArgs e)
+        {
+            openFileDialogTask_KKA.ShowDialog();
+            openFilePath = openFileDialogTask_KKA.FileName;
+
+            int[,] arrayValues = new int[rows, columns];
+
+            arrayValues = LoadFromFileData(openFilePath);
+
+            dataGridViewInput_KKA.ColumnCount = columns;
+            dataGridViewInput_KKA.RowCount = rows;
+            dataGridViewOutput_KKA.ColumnCount = columns;
+            dataGridViewOutput_KKA.RowCount = rows;
+
+            for (int i = 0; i < columns; i++)
+            {
+                dataGridViewInput_KKA.Columns[i].Width = 25;
+                dataGridViewOutput_KKA.Columns[i].Width = 25;
+            }
+            for (int r = 0; r < rows; r++)
+            {
+                for (int c = 0; c < columns; c++)
+                {
+                    dataGridViewInput_KKA.Rows[r].Cells[c].Value = arrayValues[r, c];
+                }
+            }
+            arrayValues = ds.GetMatrix(openFilePath);
+            buttonDone_KKA.Enabled = true;
+        }
+
+        private void FormMain_Load(object sender, EventArgs e)
+        {
+            dataGridViewInput_KKA.ColumnCount = 50;
+            dataGridViewOutput_KKA.ColumnCount = 50;
+
+            dataGridViewInput_KKA.RowCount = 50;
+            dataGridViewOutput_KKA.RowCount = 50;
+
+            panelInputMatrix_KKA.Width = this.Width / 2;
+
+            for (int i = 0; i < 50; i++)
+            {
+                dataGridViewInput_KKA.Columns[i].Width = 25;
+                dataGridViewOutput_KKA.Columns[i].Width = 25;
+            }
+        }
+
+        private void buttonDone_KKA_Click(object sender, EventArgs e)
+        {
+            int[,] arrayValues = new int[rows, columns];
+            arrayValues = ds.GetMatrix(openFilePath);
+
+            for (int r = 0; r < rows; r++)
+            {
+                for (int c = 0; c < columns; c++)
+                {
+                    dataGridViewOutput_KKA.Rows[r].Cells[c].Value = arrayValues[r, c];
+                }
+            }
+
+            buttonSave_KKA.Enabled = true;
+        }
+
+        private void buttonSave_KKA_Click(object sender, EventArgs e)
+        {
+            saveFileDialogMatrix_KKA.FileName = "OutPutFileTask7.csv";
+            saveFileDialogMatrix_KKA.InitialDirectory = Directory.GetCurrentDirectory();
+            saveFileDialogMatrix_KKA.ShowDialog();
+
+            string path = saveFileDialogMatrix_KKA.FileName;
+
+            FileInfo fileInfo = new FileInfo(path);
+            bool fileExists = fileInfo.Exists;
+
+            if(fileExists)
+            {
+                File.Delete(path);
+            }
+
+            int rows = dataGridViewOutput_KKA.RowCount;
+            int columns = dataGridViewOutput_KKA.ColumnCount;
+
+            string str = "";
+
+            for (int i = 0; i < rows; i++)
+            {
+                for (int j = 0; j < columns; j++)
+                {
+                    if (j != columns - 1)
+                    {
+                        str = str + dataGridViewOutput_KKA.Rows[i].Cells[j].Value + ";";
+                    } else
+                    {
+                        str = str + dataGridViewOutput_KKA.Rows[i].Cells[j].Value;
+                    }
+                }
+                File.AppendAllText(path, str + Environment.NewLine);
+                str = "";
+            }
+        }
+
+        private void buttonDone_KKA_MouseEnter(object sender, EventArgs e)
+        {
+            toolTipHelpButtons_KKA.ToolTipTitle = "Выполнить";
+        }
+
+        private void buttonOpenFile_KKA_MouseEnter(object sender, EventArgs e)
+        {
+            toolTipHelpButtons_KKA.ToolTipTitle = "Открыть файл";
+        }
+
+        private void buttonSave_KKA_MouseEnter(object sender, EventArgs e)
+        {
+            toolTipHelpButtons_KKA.ToolTipTitle = "Сохранить в файл";
+        }
+
+        private void buttonInfo_KKA_MouseEnter(object sender, EventArgs e)
+        {
+            toolTipHelpButtons_KKA.ToolTipTitle = "Справка";
+        }
+
+        private void buttonInfo_KKA_Click(object sender, EventArgs e)
+        {
+            FormAbout formAbout = new FormAbout();
+            formAbout.ShowDialog();
+        }
+    }
+}
